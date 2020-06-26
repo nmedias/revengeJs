d08a0be9089422837e92ed02e1b0ff0c0154932a
Manu <manuela@nmedias.net>
2020-06-26 12:25:35 +0200

    format and clean

--- webpack/copy-to-package.js
+++ webpack/copy-to-package.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-var-requires, no-console */
 const fs = require('fs-extra');
 const path = require('path');
 const config = require('./config');
@@ -9,8 +9,8 @@ const sourceLib = config.lib.srcPath;
 const destLib = path.join(config.package, 'lib');
 
 async function copyFiles() {
-    const green = '\x1b[32m%s\x1b[0m'
-    const cyan = '\x1b[36m%s\x1b[0m'
+    const green = '\x1b[32m%s\x1b[0m';
+    const cyan = '\x1b[36m%s\x1b[0m';
     const ascii = `
 ░░░░░░  ░░░░░░░ ░░    ░░ ░░░░░░░ ░░░    ░░  ░░░░░░  ░░░░░░░      ░░ ░░░░░░░ 
 ▒▒   ▒▒ ▒▒      ▒▒    ▒▒ ▒▒      ▒▒▒▒   ▒▒ ▒▒       ▒▒           ▒▒ ▒▒      
@@ -22,22 +22,25 @@ async function copyFiles() {
 
     try {
         await fs.remove(config.package);
-        // eslint-disable-next-line no-console
-
-        console.log(`${cyan}`, `▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-        `)
+        console.log(
+            `${cyan}`,
+            `▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+        `
+        );
 
         console.log(`${cyan}`, `Package files are going to be re-created.`);
 
         await fs.copy(sourceBuild, destBuild);
-        // eslint-disable-next-line no-console
-        console.log(`${green}`, `New files copied from /dist/${config.lib.name} to /package/${config.lib.name}.`);
+        console.log(
+            `${green}`,
+            `New files copied from /dist/${config.lib.name} to /package/${config.lib.name}.`
+        );
         await fs.copy(sourceLib, destLib);
-        // eslint-disable-next-line no-console
-        console.log(`${green}`, `New files copied from /src/lib to /package/${config.lib.name}/lib.`);
-
+        console.log(
+            `${green}`,
+            `New files copied from /src/lib to /package/${config.lib.name}/lib.`
+        );
     } catch (err) {
-        // eslint-disable-next-line no-console
         console.error(err);
     }
 }
