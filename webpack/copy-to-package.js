078eec4f17f5e33965816fd18c8ef928b971a9c5
Manu <manuela@nmedias.net>
2020-06-26 12:22:09 +0200

    Fun with Ascii

--- webpack/copy-to-package.js
+++ webpack/copy-to-package.js
@@ -8,21 +8,34 @@ const destBuild = config.package;
 const sourceLib = config.lib.srcPath;
 const destLib = path.join(config.package, 'lib');
 
-
 async function copyFiles() {
+    const green = '\x1b[32m%s\x1b[0m'
+    const cyan = '\x1b[36m%s\x1b[0m'
+    const ascii = `
+░░░░░░  ░░░░░░░ ░░    ░░ ░░░░░░░ ░░░    ░░  ░░░░░░  ░░░░░░░      ░░ ░░░░░░░ 
+▒▒   ▒▒ ▒▒      ▒▒    ▒▒ ▒▒      ▒▒▒▒   ▒▒ ▒▒       ▒▒           ▒▒ ▒▒      
+▒▒▒▒▒▒  ▒▒▒▒▒   ▒▒    ▒▒ ▒▒▒▒▒   ▒▒ ▒▒  ▒▒ ▒▒   ▒▒▒ ▒▒▒▒▒        ▒▒ ▒▒▒▒▒▒▒ 
+▓▓   ▓▓ ▓▓       ▓▓  ▓▓  ▓▓      ▓▓  ▓▓ ▓▓ ▓▓    ▓▓ ▓▓      ▓▓   ▓▓      ▓▓ 
+██   ██ ███████   ████   ███████ ██   ████  ██████  ███████  █████  ███████   `;
+
+    console.log('\x1b[36m%s\x1b[0m', ascii);
+
     try {
         await fs.remove(config.package);
         // eslint-disable-next-line no-console
-        console.log(`${config.package} was removed`);
+
+        console.log(`${cyan}`, `▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+        `)
+
+        console.log(`${cyan}`, `Package files are going to be re-created.`);
 
         await fs.copy(sourceBuild, destBuild);
         // eslint-disable-next-line no-console
-        console.log(
-            `New files copied from /dist/${config.lib.name} to /package/${config.lib.name}.`
-        );
+        console.log(`${green}`, `New files copied from /dist/${config.lib.name} to /package/${config.lib.name}.`);
         await fs.copy(sourceLib, destLib);
         // eslint-disable-next-line no-console
-        console.log(`New files copied from /src/lib to /package/${config.lib.name}/lib.`);
+        console.log(`${green}`, `New files copied from /src/lib to /package/${config.lib.name}/lib.`);
+
     } catch (err) {
         // eslint-disable-next-line no-console
         console.error(err);
